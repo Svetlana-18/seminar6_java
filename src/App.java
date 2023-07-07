9c93189d9c0cb774a14855b48daeb9b6b61bd02f
Svetlana-18 <vasilevichsvetlana@yandex.ru>
2023-07-07 22:35:49 +0300

    homework6_1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
+
+import java.text.SimpleDateFormat;
+import java.util.HashSet;
+import java.util.Locale;
+
+import Homework6.Unicornshop;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        SimpleDateFormat formatter = new SimpleDateFormat("EEE, MMMM d, yyyy", Locale.ENGLISH);
+
+        Unicornshop unicorn1 = new Unicornshop(2000, "Барсик", 245, formatter.parse("Sat, April 4, 2019"), true, 1);
+        Unicornshop unicorn2 = new Unicornshop(3000, "Кеша", 245, formatter.parse("Sat, April 4, 2010"), false, 2);
+
+        HashSet<Unicornshop> unicornSet = new HashSet<Unicornshop>();
+        unicornSet.add(unicorn1);
+        unicornSet.add(unicorn2);
+
+        System.out.println(unicorn1);
+        System.out.println(unicorn2);
+
+        System.out.println(unicorn1.equals(unicorn2));
+        System.out.println(unicornSet.contains(unicorn2));
+
     }
 }
---
+++ src/Homework6/Unicornshop.java
@@ -0,0 +1,52 @@
+package Homework6;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Objects;
+
+public class Unicornshop {
+
+    public int weight;
+    public String name;
+    public int color;
+    public Date age;
+    public boolean isMan;
+    public long id;
+
+    public Unicornshop(int weight, String name, int color, Date age, boolean isMan, long id) {
+        this.weight = weight;
+        this.name = name;
+        this.color = color;
+        this.age = age;
+        this.isMan = isMan;
+        this.id = id;
+    }
+
+    public boolean medSup() {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        SimpleDateFormat formatter = new SimpleDateFormat("EEE, MMMM d, yyyy", Locale.ENGLISH);
+
+        return "Кличка: " + name + " ID: " + id + " Вес: " + weight + " Дата рождения: " + formatter.format(age);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Unicornshop unicorn = (Unicornshop) o;
+        return weight == unicorn.weight && color == unicorn.color && isMan == unicorn.isMan;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(weight, name, color, age, isMan, id);
+    }
+
+}
